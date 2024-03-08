d0a53900f3059ef275cf967c4ca0e57d34d48fcf
Michael Mgebrishvili <mmd920802@gmail.com>
2024-03-08 22:45:07 +0300

    first commit

---
+++ src/components/ColorControl.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { cnColorControl } from './ColorControl.classname';
+
+import './App.css';
+
+
+const ColorControl = () => {
+    return (
+        <div className={cnColorControl('')}>
+        </div>
+    );
+}
+
+export { ColorControl };
